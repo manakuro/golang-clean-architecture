8aabc6a8f93186c391c5831923bc8f2d5b352acc
manato.kuroda <a7108100@gmail.com>
2019-11-07 10:10:16 +0900

    refactor: update preesnter

--- interface/presenter/user_presenter.go
+++ interface/presenter/user_presenter.go
@@ -15,7 +15,7 @@ func NewUserPresenter() UserPresenter {
 
 func (up *userPresenter) ResponseUsers(us []*model.User) []*model.User {
 	for _, u := range us {
-		u.Name = u.Name + "nyan"
+		u.Name = "Mr." + u.Name
 	}
 	return us
 }
